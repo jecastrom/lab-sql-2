f37f99afea310487b578b214a6396cca12fef66d
jecastrom <jcastro.de@outlook.com>
2021-12-29 09:46:16 +0100

    Solutions

--- files_for_lab/SQL Lab 2 solutions script only.sql
+++ files_for_lab/SQL Lab 2 solutions script only.sql
@@ -1,153 +1,110 @@
-
 /* 
-Jorge Castro DAPT NOV2021
-Lab | SQL Queries 2
-
-Instructions:
-
-Select all the actors with the first name ‘Scarlett’.
-*/
+ Jorge Castro DAPT NOV2021
+ Lab | SQL Queries 2
+ 
+ Instructions:
+ 
+ Select all the actors with the first name ‘Scarlett’.
+ */
 SELECT
   *
-FROM actor
-WHERE first_name = 'Scarlett';
-
-
-
-
-
-
+FROM
+  actor
+WHERE
+  first_name = 'Scarlett';
 /*
-Select all the actors with the last name ‘Johansson’.
-*/
+ Select all the actors with the last name ‘Johansson’.
+ */
 SELECT
   *
-FROM actor
-WHERE last_name = ('Johansson');
-
-
-
-
-
-
+FROM
+  actor
+WHERE
+  last_name = ('Johansson');
 /* 
-How many films (movies) are available for rent?
-*/
+ How many films (movies) are available for rent?
+ */
 SELECT
   COUNT(*) film_id
-FROM inventory;
-
-
-
-
-
-
+FROM
+  inventory;
 /*
-How many films have been rented?
-*/
+ How many films have been rented?
+ */
 SELECT
   COUNT(*) rental_id
-FROM rental;
-
-
-
-
-
-
+FROM
+  rental;
 /*  
-What is the shortest and longest rental period?
-*/
+ What is the shortest and longest rental period?
+ */
 SELECT
   MIN(rental_duration),
   MAX(rental_duration)
-FROM film;
-
+FROM
+  film;
 SELECT
-  MAX (DATEDIFF (rental.return_date, rental.rental_date)) AS longest_rental_period,
-(DATEDIFF (rental.return_date, rental.rental_date)) > (0) AS shortest_rental_period
-FROM rental;
-
-
-
-
-
-
+  MAX (
+    DATEDIFF (rental.return_date, rental.rental_date)
+  ) AS longest_rental_period,
+  (
+    DATEDIFF (rental.return_date, rental.rental_date)
+  ) > (0) AS shortest_rental_period
+FROM
+  rental;
 /* 
-What are the shortest and longest movie duration? 
-Name the values max_duration and min_duration.
-*/
+ What are the shortest and longest movie duration? 
+ Name the values max_duration and min_duration.
+ */
 SELECT
   MIN(length) AS min_duration,
   MAX(length) AS max_duration
-FROM film;
-
-
-
-
-
-
+FROM
+  film;
 /* 
-What's the average movie duration?
-*/
+ What's the average movie duration?
+ */
 SELECT
   ROUND(AVG(length)) AS average_movie_duration
-FROM film;
-
-
-
-
-
-
+FROM
+  film;
 /* 
-What's the average movie duration expressed in format (hours, minutes)?
-*/
-
+ What's the average movie duration expressed in format (hours, minutes)?
+ */
 /*Using the FLOOR() and MOD function*/
 /* 
-We have to round the operation first otherwise will give 55m,127
-select CONCAT(FLOOR((avg(length))/60),'h ',MOD((avg(length)),60),'m') from film;
-*/
+ We have to round the operation first otherwise will give 55m,127
+ select CONCAT(FLOOR((avg(length))/60),'h ',MOD((avg(length)),60),'m') from film;
+ */
 SELECT
-  CONCAT(FLOOR(ROUND((AVG(length))) / 60), 'h ', MOD(ROUND((AVG(length))), 60), 'm')
-  AS average_movie_duration
-FROM film;
-
-
-
-
-
-
+  CONCAT(
+    FLOOR(ROUND((AVG(length))) / 60),
+    'h ',
+    MOD(ROUND((AVG(length))), 60),
+    'm'
+  ) AS average_movie_duration
+FROM
+  film;
 /* 
-How many movies longer than 3 hours?
-*/
-SELECT DISTINCT
-  COUNT(length) AS movies_longer_than_3h
-FROM film
-WHERE length > (3 * 60);
-
-
-
-
-
-
+ How many movies longer than 3 hours?
+ */
+SELECT
+  DISTINCT COUNT(length) AS movies_longer_than_3h
+FROM
+  film
+WHERE
+  length > (3 * 60);
 /* 
-Get the name and email formatted. Example: Mary SMITH - mary.smith@sakilacustomer.org.
-*/
+ Get the name and email formatted. Example: Mary SMITH - mary.smith@sakilacustomer.org.
+ */
 SELECT
   concat(first_name, ' ', last_name, ' ', '-', ' ', email) AS customer_contact_info
-FROM customer;
-
-
-
-
-
-
+FROM
+  customer;
 /* 
-What's the length of the longest film title?
-*/
+ What's the length of the longest film title?
+ */
 SELECT
   MAX(length(title)) AS longest_film_title
-FROM film;
-
-
-
+FROM
+  film;
